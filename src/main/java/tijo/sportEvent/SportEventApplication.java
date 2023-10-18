8b6c84d68760ee208b71f1f1701cfcd1f66bd827
erykov1 <erykov1@gmail.com>
2023-10-18 20:54:45 +0200

    init project

---
+++ src/main/java/tijo/sportEvent/SportEventApplication.java
@@ -0,0 +1,13 @@
+package tijo.sportEvent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportEventApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SportEventApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tijo/sportEvent/SportEventApplicationTests.java
@@ -0,0 +1,13 @@
+package tijo.sportEvent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SportEventApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
