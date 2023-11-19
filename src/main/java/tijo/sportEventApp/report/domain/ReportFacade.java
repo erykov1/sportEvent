3e6bc0f838a8629629049e3c3c0c450105bebd22
erykov1 <erykov1@gmail.com>
2023-11-19 22:41:24 +0100

    develop fix

--- src/main/java/tijo/sportEventApp/report/domain/ReportFacade.java
+++ src/main/java/tijo/sportEventApp/report/domain/ReportFacade.java
@@ -2,16 +2,13 @@ package tijo.sportEventApp.report.domain;
 
 
 import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.experimental.FieldDefaults;
 import org.springframework.beans.factory.annotation.Autowired;
 
+@AllArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
 public class ReportFacade {
   ReportRepository reportRepository;
   SportEventAssignRepository sportEventAssignRepository;
-
-  @Autowired
-  public ReportFacade(ReportRepository reportRepository) {
-    this.reportRepository = reportRepository;
-  }
 }
